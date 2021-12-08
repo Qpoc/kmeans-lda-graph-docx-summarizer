56e0c8f3d658949bd833babb7cb2e325be99d35e
Kupoc <johncyrus.patungan.sti@gmail.com>
2021-12-09 00:01:17 +0800

    Deployment

--- summarizer/settings.py
+++ summarizer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&4-j61ev-k2+k0s23es$po54+=3ds%6&=pu2vi(p#sa7ywp5h2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['pollaplous.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
